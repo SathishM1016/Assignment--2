00250e34d5ceb195a0ca30e8a83d929d3500dcab
Sathish M <75086556+SathishM1016@users.noreply.github.com>
2021-12-28 15:55:19 +0530

    Add files via upload

---
+++ For.js
@@ -0,0 +1,35 @@
+
+
+var cars=["BMW","Audi","Volvo","Ford"]
+
+for(let i=0;i<cars.length;i++){
+    console.log(cars[i]);
+}
+
+
+var person={
+    name:"Karthik",
+    age:"27"
+}
+
+for (let x in person){
+    console.log(person[x]);
+}
+
+var cars=["BMW","Audi","Volvo","Ford"]
+
+for(let x of cars){
+    console.log(x);
+}
+
+var values=["25","26","27","28"]
+
+values.forEach(numbers);
+
+function numbers(values){
+    console.log(values);
+}
+
+
+
+
---
+++ resume.js
@@ -0,0 +1,38 @@
+
+const Resume={
+	"Personal details":{
+       
+		Name:"Arun",
+		Email:"arunres124@gmail.com",
+		Phone:6473892787,
+        Address:"South st,India",
+		Degree:"B.Tech",
+		website:"Your web url",
+	},
+
+	"Education":[
+		{
+			SSLC:"Some school", Grade:8,
+		},
+		{
+			HSC:"Some School", Grade:8,
+		},
+	],
+
+    "Interest":[
+		{
+			name:"Sports", specific:"Cricket",
+		},
+		{
+			name:"Reading" ,specific:"Books",
+		},
+	],
+	"Languages":[
+		{
+			name:"English",fluency:"Good",
+		},
+		{
+			name:"Tamil",fluency:"Strong",
+		},
+	]
+}
